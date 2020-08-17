e9850304e4bb321bd687496fe699acfa97ad3b88
td4b <edwin.twest@gmail.com>
2020-08-17 11:33:33 -0700

    Merged new version.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 
 ENV DEBIAN_FRONTEND=noninteractive
 ENV LANG=C.UTF-8
-EXPOSE 9392
+
 COPY install-pkgs.sh /install-pkgs.sh
 
 RUN bash /install-pkgs.sh
@@ -11,19 +11,18 @@ ENV gvm_libs_version="v11.0.1" \
     openvas_scanner_version="v7.0.1" \
     gvmd_version="v9.0.1" \
     gsa_version="v9.0.1" \
-    gvm_tools_version="v2.1.0" \
+    gvm_tools_version="2.1.0" \
     openvas_smb="v1.0.5" \
     open_scanner_protocol_daemon="v2.0.1" \
     ospd_openvas="v1.0.1" \
-    python_gvm_version="v1.5.0"
-
-RUN echo "Starting Build..." && mkdir /build
+    python_gvm_version="1.6.0"
 
     #
     # install libraries module for the Greenbone Vulnerability Management Solution
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/gvm-libs/archive/$gvm_libs_version.tar.gz && \
     tar -zxf $gvm_libs_version.tar.gz && \
     cd /build/*/ && \
@@ -32,14 +31,15 @@ RUN cd /build && \
     cmake -DCMAKE_BUILD_TYPE=Release .. && \
     make && \
     make install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
 
     #
     # install smb module for the OpenVAS Scanner
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/openvas-smb/archive/$openvas_smb.tar.gz && \
     tar -zxf $openvas_smb.tar.gz && \
     cd /build/*/ && \
@@ -48,14 +48,15 @@ RUN cd /build && \
     cmake -DCMAKE_BUILD_TYPE=Release .. && \
     make && \
     make install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
     
     #
     # Install Greenbone Vulnerability Manager (GVMD)
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/gvmd/archive/$gvmd_version.tar.gz && \
     tar -zxf $gvmd_version.tar.gz && \
     cd /build/*/ && \
@@ -64,14 +65,15 @@ RUN cd /build && \
     cmake -DCMAKE_BUILD_TYPE=Release .. && \
     make && \
     make install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
     
     #
     # Install Open Vulnerability Assessment System (OpenVAS) Scanner of the Greenbone Vulnerability Management (GVM) Solution
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/openvas-scanner/archive/$openvas_scanner_version.tar.gz && \
     tar -zxf $openvas_scanner_version.tar.gz && \
     cd /build/*/ && \
@@ -80,14 +82,15 @@ RUN cd /build && \
     cmake -DCMAKE_BUILD_TYPE=Release .. && \
     make && \
     make install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
     
     #
     # Install Greenbone Security Assistant (GSA)
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/gsa/archive/$gsa_version.tar.gz && \
     tar -zxf $gsa_version.tar.gz && \
     cd /build/*/ && \
@@ -96,67 +99,50 @@ RUN cd /build && \
     cmake -DCMAKE_BUILD_TYPE=Release .. && \
     make && \
     make install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
     
     #
     # Install Greenbone Vulnerability Management Python Library
     #
-RUN python3 -m pip install python-gvm
     
-#RUN cd /build && \
-    #wget --no-verbose https://github.com/greenbone/python-gvm/archive/$python_gvm_version.tar.gz && \
-    #tar -zxf $python_gvm_version.tar.gz && \
-    #cd /build/*/ && \
-    #python3 setup.py install && \
-    #cd /build && \
-    #rm -rf *
+RUN pip3 install python-gvm==$python_gvm_version
     
     #
     # Install Open Scanner Protocol daemon (OSPd)
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/ospd/archive/$open_scanner_protocol_daemon.tar.gz && \
     tar -zxf $open_scanner_protocol_daemon.tar.gz && \
     cd /build/*/ && \
     python3 setup.py install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
     
     #
     # Install Open Scanner Protocol for OpenVAS
     #
     
-RUN cd /build && \
+RUN mkdir /build && \
+    cd /build && \
     wget --no-verbose https://github.com/greenbone/ospd-openvas/archive/$ospd_openvas.tar.gz && \
     tar -zxf $ospd_openvas.tar.gz && \
     cd /build/*/ && \
     python3 setup.py install && \
-    cd /build && \
-    rm -rf *
+    cd / && \
+    rm -rf /build
     
     #
     # Install GVM-Tools
     #
-RUN python3 -m pip install gvm-tools    
-#RUN cd /build && \
-    #wget --no-verbose https://github.com/greenbone/gvm-tools/archive/$gvm_tools_version.tar.gz && \
-    #tar -zxf $gvm_tools_version.tar.gz && \
-    #cd /build/*/ && \
-    #python3 setup.py install && \
-    #echo "/usr/local/lib" > /etc/ld.so.conf.d/openvas.conf && ldconfig && cd / && rm -rf /build
-#
+    
+RUN pip3 install gvm-tools==$gvm_tools_version && \
+    echo "/usr/local/lib" > /etc/ld.so.conf.d/openvas.conf && ldconfig && cd / && rm -rf /build
 
-    # 
-    # Make sure all libraries are linked and add a random directory suddenly needed by ospd :/
-    #
-
-RUN ldconfig && \
-    mkdir /var/run/ospd
+COPY sshd_config /sshd_config
 
 COPY scripts/* /
-
-RUN chmod +x start.sh && chmod +x cert-data-sync.sh && chmod +x reportFix.sh && chmod +x scap-data-sync.sh
 
 CMD '/start.sh'
--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,14 @@ set -Eeuo pipefail
 
 USERNAME=${USERNAME:-admin}
 PASSWORD=${PASSWORD:-admin}
-RELAYHOST=${RELAYHOST:-172.17.0.1}
+TIMEOUT=${TIMEOUT:-15}
+RELAYHOST=${RELAYHOST:-smtp}
 SMTPPORT=${SMTPPORT:-25}
+
+HTTPS=${HTTPS:-true}
+TZ=${TZ:-UTC}
+SSHD=${SSHD:-false}
+DB_PASSWORD=${DB_PASSWORD:-none}
 
 if [ ! -d "/run/redis" ]; then
 	mkdir /run/redis
@@ -12,7 +18,7 @@ fi
 if  [ -S /run/redis/redis.sock ]; then
         rm /run/redis/redis.sock
 fi
-redis-server --unixsocket /run/redis/redis.sock --unixsocketperm 700 --timeout 0 --databases 128 --maxclients 512 --daemonize yes --port 6379 --bind 0.0.0.0
+redis-server --unixsocket /run/redis/redis.sock --unixsocketperm 700 --timeout 0 --databases 128 --maxclients 4096 --daemonize yes --port 6379 --bind 0.0.0.0
 
 echo "Wait for redis socket to be created..."
 while  [ ! -S /run/redis/redis.sock ]; do
@@ -36,49 +42,46 @@ fi
 
 if  [ ! -d /data/database ]; then
 	echo "Creating Database folder..."
-	mv /var/lib/postgresql/10/main /data/database
-	ln -s /data/database /var/lib/postgresql/10/main
-	chown postgres:postgres -R /var/lib/postgresql/10/main
+	mkdir /data/database
 	chown postgres:postgres -R /data/database
+	su -c "/usr/lib/postgresql/12/bin/initdb /data/database" postgres
 fi
 
-if [ ! -L /var/lib/postgresql/10/main ]; then
-	echo "Fixing Database folder..."
-	rm -rf /var/lib/postgresql/10/main
-	ln -s /data/database /var/lib/postgresql/10/main
-	chown postgres:postgres -R /var/lib/postgresql/10/main
-	chown postgres:postgres -R /data/database
-fi
-
-if [ ! -L /usr/local/var/lib  ]; then
-	echo "Fixing local/var/lib ... "
-	if [ ! -d /data/var-lib ]; then  mkdir /data/var-lib; fi
-	cp -rf /usr/local/var/lib/* /data/var-lib
-	rm -rf /usr/local/var/lib
-	ln -s /data/var-lib /usr/local/var/lib
-fi
-if [ ! -L /usr/local/share ]; then
-	echo "Fixing local/share ... "
-	if [ ! -d /data/local-share ]; then mkdir /data/local-share; fi
-	cp -rf /usr/local/share/* /data/local-share/
-	rm -rf /usr/local/share 
-	ln -s /data/local-share /usr/local/share 
-fi
+chown postgres:postgres -R /data/database
 
 echo "Starting PostgreSQL..."
-/usr/bin/pg_ctlcluster --skip-systemctl-redirect 10 main start
+su -c "/usr/lib/postgresql/12/bin/pg_ctl -D /data/database start" postgres
+
+if  [ ! -d /data/ssh ]; then
+	echo "Creating SSH folder..."
+	mkdir /data/ssh
+	
+	rm -rf /etc/ssh/ssh_host_*
+	
+	dpkg-reconfigure openssh-server
+	
+	mv /etc/ssh/ssh_host_* /data/ssh/
+fi
+
+if  [ ! -h /etc/ssh ]; then
+	rm -rf /etc/ssh
+	ln -s /data/ssh /etc/ssh
+fi
 
 if [ ! -f "/firstrun" ]; then
 	echo "Running first start configuration..."
+	
+	ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 	echo "Creating Openvas NVT sync user..."
 	useradd --home-dir /usr/local/share/openvas openvas-sync
 	chown openvas-sync:openvas-sync -R /usr/local/share/openvas
 	chown openvas-sync:openvas-sync -R /usr/local/var/lib/openvas
+
 	echo "Creating Greenbone Vulnerability system user..."
 	useradd --home-dir /usr/local/share/gvm gvm
 	chown gvm:gvm -R /usr/local/share/gvm
-	if [ ! -d /usr/local/var/lib/gvm/cert-data ]; then mkdir -p /usr/local/var/lib/gvm/cert-data; fi
+	mkdir /usr/local/var/lib/gvm/cert-data
 	chown gvm:gvm -R /usr/local/var/lib/gvm
 	chmod 770 -R /usr/local/var/lib/gvm
 	chown gvm:gvm -R /usr/local/var/log/gvm
@@ -86,6 +89,7 @@ if [ ! -f "/firstrun" ]; then
 
 	adduser openvas-sync gvm
 	adduser gvm openvas-sync
+	
 	touch /firstrun
 fi
 
@@ -96,19 +100,110 @@ if [ ! -f "/data/firstrun" ]; then
 	su -c "psql --dbname=gvmd --command='create role dba with superuser noinherit;'" postgres
 	su -c "psql --dbname=gvmd --command='grant dba to gvm;'" postgres
 	su -c "psql --dbname=gvmd --command='create extension \"uuid-ossp\";'" postgres
+	
+	echo "listen_addresses = '*'" >> /data/database/postgresql.conf
+	echo "port = 5432" >> /data/database/postgresql.conf
+	
+	echo "host    all             all              0.0.0.0/0                 md5" >> /data/database/pg_hba.conf
+	echo "host    all             all              ::/0                      md5" >> /data/database/pg_hba.conf
+	
+	chown postgres:postgres -R /data/database
+	
+	su -c "/usr/lib/postgresql/12/bin/pg_ctl -D /data/database restart" postgres
+	
 	touch /data/firstrun
 fi
 
-echo "Updating NVTs..."
-su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.openvas.org:/nvt-feed /usr/local/var/lib/openvas/plugins" openvas-sync
-sleep 5
+su -c "gvmd --migrate" gvm
 
-echo "Updating CERT data..."
-su -c "/cert-data-sync.sh" openvas-sync
-sleep 5
+if [ $DB_PASSWORD == "none" ]; then
+	su -c "psql --dbname=gvmd --command=\"alter user gvm password '$DB_PASSWORD';\"" postgres
+fi
 
-echo "Updating SCAP data..."
-su -c "/scap-data-sync.sh" openvas-sync
+if  [ ! -d /data/gvmd ]; then
+	echo "Creating gvmd folder..."
+	mkdir /data/gvmd
+	chown gvm:gvm -R /data/gvmd
+fi
+
+if  [ ! -h /usr/local/var/lib/gvm/gvmd ]; then
+	echo "Fixing gvmd folder..."
+	rm -rf /usr/local/var/lib/gvm/gvmd
+	ln -s /data/gvmd /usr/local/var/lib/gvm/gvmd
+fi
+
+if  [ ! -d /data/certs ]; then
+	echo "Creating certs folder..."
+	mkdir -p /data/certs/CA
+	mkdir -p /data/certs/private
+	
+	echo "Generating certs..."
+	gvm-manage-certs -a
+	
+	cp /usr/local/var/lib/gvm/CA/* /data/certs/CA/
+	
+	cp -r /usr/local/var/lib/gvm/private/* /data/certs/private/
+	
+	chown gvm:gvm -R /data/certs
+fi
+
+if [ ! -h /usr/local/var/lib/gvm/CA ]; then
+	echo "Fixing certs CA folder..."
+	rm -rf /usr/local/var/lib/gvm/CA
+	ln -s /data/certs/CA /usr/local/var/lib/gvm/CA
+	chown gvm:gvm -R /data/certs
+	chown gvm:gvm -R /usr/local/var/lib/gvm/CA
+fi
+
+if [ ! -h /usr/local/var/lib/gvm/private ]; then
+	echo "Fixing certs private folder..."
+	rm -rf /usr/local/var/lib/gvm/private
+	ln -s /data/certs/private /usr/local/var/lib/gvm/private
+	chown gvm:gvm -R /data/certs
+	chown gvm:gvm -R /usr/local/var/lib/gvm/private
+fi
+
+if  [ ! -d /data/plugins ]; then
+	echo "Creating NVT Plugins folder..."
+	mkdir /data/plugins
+fi
+
+if [ ! -h /usr/local/var/lib/openvas/plugins ]; then
+	echo "Fixing NVT Plugins folder..."
+	rm -rf /usr/local/var/lib/openvas/plugins
+	ln -s /data/plugins /usr/local/var/lib/openvas/plugins
+	chown openvas-sync:openvas-sync -R /data/plugins
+	chown openvas-sync:openvas-sync -R /usr/local/var/lib/openvas/plugins
+fi
+
+if  [ ! -d /data/cert-data ]; then
+	echo "Creating CERT Feed folder..."
+	mkdir /data/cert-data
+fi
+
+if [ ! -h /usr/local/var/lib/gvm/cert-data ]; then
+	echo "Fixing CERT Feed folder..."
+	rm -rf /usr/local/var/lib/gvm/cert-data
+	ln -s /data/cert-data /usr/local/var/lib/gvm/cert-data
+	chown openvas-sync:openvas-sync -R /data/cert-data
+	chown openvas-sync:openvas-sync -R /usr/local/var/lib/gvm/cert-data
+fi
+
+if  [ ! -d /data/scap-data ]; then
+	echo "Creating SCAP Feed folder..."
+	mkdir /data/scap-data
+fi
+
+if [ ! -h /usr/local/var/lib/gvm/scap-data ]; then
+	echo "Fixing SCAP Feed folder..."
+	rm -rf /usr/local/var/lib/gvm/scap-data
+	ln -s /data/scap-data /usr/local/var/lib/gvm/scap-data
+	chown openvas-sync:openvas-sync -R /data/scap-data
+	chown openvas-sync:openvas-sync -R /usr/local/var/lib/gvm/scap-data
+fi
+
+# Sync NVTs, CERT data, and SCAP data on container start
+/sync-all.sh
 
 if [ -f /var/run/ospd.pid ]; then
   rm /var/run/ospd.pid
@@ -117,13 +212,14 @@ fi
 if [ -S /tmp/ospd.sock ]; then
   rm /tmp/ospd.sock
 fi
-echo "Starting Postfix for report delivery by email"
-# Configure postfix
-sed -i "s/^relayhost.*$/relayhost = ${RELAYHOST}:${SMTPPORT}/" /etc/postfix/main.cf
-# Start the postfix  bits
-#/usr/lib/postfix/sbin/master -w
-service postfix start
 
+if [ ! -d /var/run/ospd ]; then
+  mkdir /var/run/ospd
+fi
+
+echo "Starting Postfix for report delivery by email"
+sed -i "s/^relayhost.*$/relayhost = ${RELAYHOST}:${SMTPPORT}/" /etc/postfix/main.cf
+service postfix start
 
 echo "Starting Open Scanner Protocol daemon for OpenVAS..."
 ospd-openvas --log-file /usr/local/var/log/gvm/ospd-openvas.log --unix-socket /tmp/ospd.sock --log-level INFO
@@ -132,25 +228,10 @@ while  [ ! -S /tmp/ospd.sock ]; do
 	sleep 1
 done
 
-# This is cludgy and needs a better fix. namely figure out how to hard code alllll of the scoket references in the startup process.
-# It's possible this is a problem with my DB as I only see it when using my DB. 
-if [ ! -L /var/run/openvassd.sock ]; then
-	echo "Fixing the ospd socket ..."
-	rm -f /var/run/openvassd.sock
-	ln -s /tmp/ospd.sock /var/run/openvassd.sock
-fi
-
 chmod 666 /tmp/ospd.sock
-echo "Migrating database if needed"
-su -c "gvmd -m" gvm
-echo "Starting Greenbone Vulnerability Manager..."
-su -c "gvmd --osp-vt-update=/tmp/ospd.sock" gvm
 
-if [ ! -L /var/run/gvmd.sock ]; then
-	echo "Fixing the gvmd socket ..."
-	rm -f /var/run/gvmd.sock
-	ln -s /usr/local/var/run/gvmd.sock /var/run/gvmd.sock
-fi
+echo "Starting Greenbone Vulnerability Manager..."
+su -c "gvmd --listen=0.0.0.0 --port=9390" gvm
 
 until su -c "gvmd --get-users" gvm; do
 	sleep 1
@@ -158,17 +239,54 @@ done
 
 if [ ! -f "/data/created_gvm_user" ]; then
 	echo "Creating Greenbone Vulnerability Manager admin user"
-	su -c "gvmd --create-user=${USERNAME} --password=${PASSWORD}" gvm
+	su -c "gvmd --role=\"Super Admin\" --create-user=\"$USERNAME\" --password=\"$PASSWORD\"" gvm
 	
 	touch /data/created_gvm_user
 fi
 
 echo "Starting Greenbone Security Assistant..."
-su -c "gsad --verbose --http-only --no-redirect --port=9392" gvm
+if [ $HTTPS == "true" ]; then
+	su -c "gsad --verbose --gnutls-priorities=SECURE128:-AES-128-CBC:-CAMELLIA-128-CBC:-VERS-SSL3.0:-VERS-TLS1.0 --timeout=$TIMEOUT --no-redirect --mlisten=127.0.0.1 --mport=9390 --port=9392" gvm
+else
+	su -c "gsad --verbose --http-only --timeout=$TIMEOUT --no-redirect --mlisten=127.0.0.1 --mport=9390 --port=9392" gvm
+fi
+
+if [ $SSHD == "true" ]; then
+	echo "Starting OpenSSH Server..."
+	
+	if  [ ! -d /data/scanner-ssh-keys ]; then
+		echo "Creating scanner SSH keys folder..."
+		mkdir /data/scanner-ssh-keys
+		chown gvm:gvm -R /data/scanner-ssh-keys
+	fi
+	if [ ! -h /usr/local/share/gvm/.ssh ]; then
+		echo "Fixing scanner SSH keys folder..."
+		rm -rf /usr/local/share/gvm/.ssh
+		ln -s /data/scanner-ssh-keys /usr/local/share/gvm/.ssh
+		chown gvm:gvm -R /data/scanner-ssh-keys
+		chown gvm:gvm -R /usr/local/share/gvm/.ssh
+	fi
+	
+	if [ ! -d /sockets ]; then
+		mkdir /sockets
+		chown gvm:gvm -R /sockets
+	fi
+	
+	echo "gvm:gvm" | chpasswd
+	
+	rm -rf /var/run/sshd
+	mkdir -p /var/run/sshd
+	
+	/usr/sbin/sshd -f /sshd_config -E /usr/local/var/log/gvm/sshd.log
+fi
 
 echo "++++++++++++++++++++++++++++++++++++++++++++++"
 echo "+ Your GVM 11 container is now ready to use! +"
 echo "++++++++++++++++++++++++++++++++++++++++++++++"
+echo ""
+echo "-----------------------------------------------------------"
+echo "Server Public key: $(cat /etc/ssh/ssh_host_ed25519_key.pub)"
+echo "-----------------------------------------------------------"
 echo ""
 echo "++++++++++++++++"
 echo "+ Tailing logs +"
